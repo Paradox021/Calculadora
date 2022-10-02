0840a8ab80f47e8426d442c6b3222a56420f08a7
Paradox021 <espanacarmelo@gmail.com>
2022-10-02 13:43:24 +0200

    añadir teclado

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.Numero');
 const botonesOperadores = document.querySelectorAll('.Operador');
-
+const body = document.getElementById('body');
 const display = new Display(displayValorAnterior, displayValorActual);
+const operadores = {'+':'sumar','/':'dividir','-':'restar','x':'multiplicar', '*':'multiplicar','Enter':'igual', '=':'igual'};
+const op = ['+','/','-','x','*', 'Enter', '='];
 
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
@@ -13,3 +15,18 @@ botonesNumeros.forEach(boton => {
 botonesOperadores.forEach(boton => {
     boton.addEventListener('click', () => display.computar(boton.value))
 })
+
+function agregar(event){
+    console.log(event.key)
+  if(op.indexOf(event.key) ===-1 && event.key!=='.' && isNaN(event.key)  ){
+    return;
+  }
+  if(op.indexOf(event.key)!==-1){
+    display.computar((operadores[event.key]))
+  }
+  if(!isNaN(event.key)|| event.key==='.'){
+    display.agregarNumero(event.key)
+  }
+}
+
+body.addEventListener('keypress', agregar)
